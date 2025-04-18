8d0e49e3b3e91b2483ed377d973204b7504512ba
bm1611 <bm1611@naver.com>
2025-04-18 12:13:13 +0900

    null-safe 적용

--- main/java/com/example/admin/dto/PostResponse.java
+++ main/java/com/example/admin/dto/PostResponse.java
@@ -1,13 +1,25 @@
 package com.example.admin.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
-@AllArgsConstructor
+@Setter
+@NoArgsConstructor
 public class PostResponse {
+
     private Long id;
     private String title;
     private String content;
     private String author;
+
+    // 직접 구현한 null-safe 생성자만 사용
+    public PostResponse(Long id, String title, String content, String author) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.author = (author != null) ? author : "알 수 없음"; // null-safe 적용부분분 
+    }
 }
+
